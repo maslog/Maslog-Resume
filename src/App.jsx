be72794656e4daf0c91cb43a2ab027e2b1246b04
maslog <ronaldmaslog5@gmail.com>
2024-02-22 22:32:50 +0800

    My seventh Commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
-
+import {useState} from "react"
 import './App.css'
 
 function App() {
+
+  const [count, setCount] = useState(0);
   
 
   return (
     <>
       <h1>RONALD MASLOG</h1>
+      <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
     </>
   )
 }
